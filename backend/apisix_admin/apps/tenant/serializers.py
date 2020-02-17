f831dd4e0e20eeb1504bef7e214406c2d179c173
tea <40557944+cctv86@users.noreply.github.com>
2020-02-17 18:38:41 +0800

    Update serializers.py

--- backend/apisix_admin/apps/tenant/serializers.py
+++ backend/apisix_admin/apps/tenant/serializers.py
@@ -3,13 +3,13 @@ from .models import Tenant, TenantGroup
 
 
 class TenantSerializer(serializers.ModelSerializer):
-    url = serializers.CharField(required=True, max_length=300, label="url地址")
-    location = serializers.CharField(required=True, max_length=300, label="位置")
+    url = serializers.CharField(required=True, max_length=250, label="url地址")
+    location = serializers.CharField(required=True, max_length=250, label="位置")
     authType = serializers.ChoiceField(choices=["JWT", "Session", "NoAuth"], label="认证类型")
     username = serializers.CharField(max_length=32, label="用户名", allow_blank=True, allow_null=True)
     password = serializers.CharField(max_length=32, label="密码", allow_blank=True, allow_null=True)
     token = serializers.CharField(max_length=32, label="token", allow_blank=True, allow_null=True)
-    desc = serializers.CharField(max_length=500, label="描述")
+    desc = serializers.CharField(max_length=250, label="描述")
 
     class Meta:
         model = Tenant
