ab6a4f8dd717ce66f5fd5f2685e5107bbc162496
tea <40557944+cctv86@users.noreply.github.com>
2020-02-17 18:38:17 +0800

    Update models.py

--- backend/apisix_admin/apps/tenant/models.py
+++ backend/apisix_admin/apps/tenant/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 
 class Tenant(models.Model):
-    url = models.CharField(max_length=300, unique=True, verbose_name="url地址")
-    location = models.CharField(max_length=300, verbose_name="位置")
+    url = models.CharField(max_length=250, unique=True, verbose_name="url地址")
+    location = models.CharField(max_length=250, verbose_name="位置")
     AUTH_CHOICES = (
         (u"NoAuth", "NoAuth"),
         (u"JWT", "JWT"),
@@ -14,7 +14,7 @@ class Tenant(models.Model):
     username = models.CharField(max_length=32, null=True, blank=True, verbose_name="用户名")
     password = models.CharField(max_length=100, null=True, blank=True, verbose_name="密码")
     token = models.CharField(max_length=100, null=True, blank=True, verbose_name="token")
-    desc = models.CharField(max_length=500, null=True, blank=True, verbose_name="描述")
+    desc = models.CharField(max_length=250, null=True, blank=True, verbose_name="描述")
 
     class Meta:
         verbose_name = "tenant"
