50be0ba2e60ddbf0a949cd2b7059ab3731f0cc94
tea <40557944+cctv86@users.noreply.github.com>
2020-02-18 09:22:33 +0800

    cors

--- backend/apisix_admin/apisix_admin/settings.py
+++ backend/apisix_admin/apisix_admin/settings.py
@@ -81,7 +81,33 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'apisix_admin.wsgi.application'
+
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
